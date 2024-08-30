84821169c2c1eec0654e7a524a1591a728e2a499
maxmwangikenya <maxmwangikenya@gmail.com>
2024-08-30 11:16:09 +0300

    first commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './components/Greet';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+//function Greet(){
+//    return <h>Hello vishwal</h>
+//}
+const Greet = () => <h1>Hello vishwas</h1>
+export default Greet
